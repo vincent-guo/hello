109b1d52c704a62e5fa0b0b5a2900757c3c45b51
vincent-guo <vincent.weiguo@gmail.com>
2022-12-08 18:12:35 +0800

    Assigning layouts in controllers

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -4,7 +4,9 @@ defmodule HelloWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> put_root_layout(:admin)
+    |> render(:home)
   end
 
   plug HelloWeb.Plugs.Locale, "en" when action in [:index]
