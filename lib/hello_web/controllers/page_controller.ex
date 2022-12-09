422a7ab4134fa00fdfe7792d1a31a80776499d44
vincent-guo <vincent.weiguo@gmail.com>
2022-12-09 10:16:29 +0800

    Setting the Content Type

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -5,8 +5,8 @@ defmodule HelloWeb.PageController do
     # The home page is often custom made,
     # so skip the default app layout.
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(201, "123")
+    |> put_resp_content_type("text/xml")
+    |> render(:index, content: "some_xml_content")
   end
 
   plug HelloWeb.Plugs.Locale, "en" when action in [:index]
