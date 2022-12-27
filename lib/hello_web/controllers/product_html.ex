6fc20855e56f821e2aeb65b8727b8d619645cf73
vincent-guo <vincent.weiguo@gmail.com>
2022-12-27 09:57:01 +0800

    Update product view

--- lib/hello_web/controllers/product_html.ex
+++ lib/hello_web/controllers/product_html.ex
@@ -1,5 +1,20 @@
 defmodule HelloWeb.ProductHTML do
   use HelloWeb, :html
 
+  import Phoenix.HTML.Form
+
+  def category_select(f, changeset) do
+    existing_ids =
+      changeset
+      |> Ecto.Changeset.get_change(:categories, [])
+      |> Enum.map(& &1.data.id)
+
+    category_opts =
+      for cat <- Hello.Catalog.list_categories(),
+          do: [key: cat.title, value: cat.id, selected: cat.id in existing_ids]
+
+    multiple_select(f, :category_ids, category_opts)
+  end
+
   embed_templates "product_html/*"
 end
